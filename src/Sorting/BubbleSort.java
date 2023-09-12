f35625598535ae6e36dadc04a4e1f8ea93cb8b13
Nikhil Patwari <nikhilp@lentra.ai>
2023-09-12 20:32:45 +0530

    Added Bubble Soet Algo

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,17 @@
+package Sorting;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        for(int i = 0; i< arr.length -1; i++) {
+            int min = i;
+            for(int j = i+1; j< arr.length; j++) {
+                if(arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
