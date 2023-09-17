2d1b85661694ef71bb1b89a185b8054c0f4c84ad
Nikhil Patwari <nikhilp@lentra.ai>
2023-09-17 10:00:47 +0530

    Added optimal solution to remove words string

--- src/String/Reverse_Words_In_String.java
+++ src/String/Reverse_Words_In_String.java
@@ -3,7 +3,7 @@ package String;
 public class Reverse_Words_In_String {
     public static void main(String[] args) {
         Reverse_Words_In_String r = new Reverse_Words_In_String();
-        System.out.println(r.reverseWords("  hello world  "));
+        System.out.println(r.reverseWordsOptimal("  hello world  "));
     }
 
     public String reverseWords(String s) {
@@ -20,5 +20,34 @@ public class Reverse_Words_In_String {
             }
         }
         return sb.toString();
+    }
+
+    // Most optimal approach
+
+    public String reverseWordsOptimal(String s) {
+        StringBuilder res = new StringBuilder();
+        StringBuilder temp = new StringBuilder();
+        char c;
+        for(int i = 0; i< s.length(); i++) {
+            c = s.charAt(i);
+            if(c != ' ') {
+                temp.append(c);
+            }else {
+                if(!temp.isEmpty()) {
+                    if(!res.isEmpty()) {
+                        res.insert(0," ");
+                    }
+                    res.insert(0, temp);
+                }
+                temp.delete(0,temp.length());
+            }
+        }
+        if(!temp.isEmpty()) {
+            if(!res.isEmpty()) {
+                res.insert(0," ");
+            }
+            res.insert(0, temp);
+        }
+        return res.toString();
     }
 }
